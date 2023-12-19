5697de4eda3abd7fd51647aea6e92f497ea56e20
David Aparicio gonzalez <daparici@c2r10s5.42madrid.com>
2023-12-19 21:47:12 +0100

    sd

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: daparici <daparici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 18:43:26 by daparici          #+#    #+#             */
-/*   Updated: 2023/12/18 20:54:01 by daparici         ###   ########.fr       */
+/*   Updated: 2023/12/19 21:39:57 by daparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,26 @@
 typedef struct philo
 {
 	int					id;
-	int					time_fork;
-}	t_philo;
-
-typedef struct dat
-{
-	int					philos_nb;
 	int					t_to_die;
 	int					t_to_eat;
 	int					t_to_sleep;
-	int					nb_of_lunchs;
+	int					*stop;
+	int					*lunchs_nb;
+	int					time_fork;
+	int					*dat;
+	pthread_mutex_t		*print_lock;
+}	t_philo;
+
+typedef struct data
+{
+	int					philos_nb;
+	int					lunchs_nb;
 	int					stop;
+	long long			star_time;
+	long long			last_time_eat;
 	pthread_t			*philosophers;
 	pthread_mutex_t		*forks;
+	pthread_mutex_t		*print_lock;
 	t_philo				*philo;
 }	t_data;
 
@@ -42,5 +49,6 @@ int		check_args(int ac, char **av);
 int		ft_atoi_p(char *str);
 int		innit_data(t_data *data, char **av, int ac);
 void	ft_free(t_data *data);
+int		init_philo_params(t_data data, int ac, char **av, int i);
 
 #endif
--- philosophers.c
+++ philosophers.c
@@ -6,7 +6,7 @@
 /*   By: daparici <daparici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 18:40:38 by daparici          #+#    #+#             */
-/*   Updated: 2023/12/18 20:55:13 by daparici         ###   ########.fr       */
+/*   Updated: 2023/12/19 21:35:10 by daparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,8 @@ int	innit_data(t_data *data, char **av, int ac)
 	if (!data->philo)
 		return (free(data->forks), free(data->philo), 0);
 	data->stop = 1;
-	data->t_to_die = ft_atoi_p(av[2]);
-	data->t_to_eat = ft_atoi_p(av[3]);
-	data->t_to_sleep = ft_atoi_p(av[4]);
 	if (ac == 6)
-		data->nb_of_lunchs = ft_atoi_p(av[5]) * data->philos_nb;
+		data->lunchs_nb = ft_atoi_p(av[5]) * data->philos_nb;
 	return (1);
 }
 
@@ -61,23 +58,60 @@ int	check_args(int ac, char **av)
 	return (1);
 }
 
+int	init_philo_params(t_data *data, int ac, char **av, int i)
+{
+	if (pthread_mutex_init(&data->forks[i], NULL))
+		return (0);
+	data->philo[i].id = i;
+	data->philo[i].t_to_die = ft_atoi_p(av[2]);
+	data->philo[i].t_to_eat = ft_atoi_p(av[3]);
+	data->philo[i].t_to_sleep = ft_atoi_p(av[4]);
+	return (1);
+}
+
+void	*rutine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	printf("%i\n", philo->id);
+	return (NULL);
+}
+
 int	create_threads(t_data *data, int ac, char **av)
 {
 	int	i;
 
 	i = 0;
-	while ()
+	data->philo->dat = data;
+	while (i < data->philos_nb)
+	{
+		if (!init_philo_params(data, ac, av, i))
+			return (0);
+		if (pthread_create(&data->philosophers[i], NULL,
+			rutine, &data->philo[i]) < 0)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 int	main(int ac, char **av)
 {
 	t_data	data;
+	int		i;
 
+	i = 0;
 	if (!check_args(ac, av))
 		return (printf("Error\n"));
 	if (!innit_data(&data, av, ac))
 		return (printf("Malloc Error\n"));
 	if (!create_threads(&data, ac, av))
 		return (ft_free(&data), 0);
+	while (i < data.philos_nb)
+	{
+		pthread_join(data.philosophers[i], NULL);
+		i++;
+	}
 	return (0);
 }
